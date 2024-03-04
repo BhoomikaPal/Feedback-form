ead9db0581d14586d3b84d2eb7e9b6e6a71c03c9
Bhoomika Pal <71694381+BhoomikaPal@users.noreply.github.com>
2024-03-04 18:41:50 +0530

    Update ThankYou.jsx

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,18 +1,47 @@
-
+// ThankYou.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './ThankYou.css';
+import { useLocation, Link } from 'react-router-dom';
+import './Home.css';
 
 function ThankYou() {
+  const location = useLocation();
+  const { feedback } = location.state || {};
+
+ 
+
+  const handleDownload = () => {
+    // Generate CSV content
+    const csvContent = `Name,Digital Initiatives,Cleanliness,Complaints,Satisfaction\n${feedback.name},${feedback.digitalInitiatives},${feedback.cleanliness},${feedback.complaints},${feedback.satisfaction}\n`;
+
+    // Create a Blob containing the CSV data
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+
+    // Create a temporary URL for the Blob
+    const url = window.URL.createObjectURL(blob);
+
+    // Create a temporary <a> element to trigger the download
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'feedback.csv');
+    document.body.appendChild(link);
+
+    // Trigger the download
+    link.click();
+
+    // Clean up resources
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="background">
-      {/* ThankYou content */}
       <div className="thank-you-content">
         <h1>Thank You for Visiting Us!</h1>
         <p>We appreciate your time and feedback.</p>
         <Link to="/feedback">
-          <button className="feedback-button">Leave Feedback</button>
-        </Link>
+            <button className="feedback-button">Leave Feedback</button>
+          </Link>
+        <button onClick={handleDownload}>Download Feedback</button>
       </div>
     </div>
   );
